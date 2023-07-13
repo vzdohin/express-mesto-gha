ef10c7fcd0e22f16e7f969ca7c927fd5c304cf32
vzdohin <peenky88@yandex.ru>
2023-07-14 00:58:31 +0500

    ПР13 v1.0: настройка проекта

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/mestodb ', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
