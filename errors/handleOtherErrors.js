bc8a5afba891968b31a128095d32204b9b9113ec
vzdohin <peenky88@yandex.ru>
2023-07-28 16:48:41 +0500

    ПР14. v1.0 правка ошибок теста #24

--- errors/handleOtherErrors.js
+++ errors/handleOtherErrors.js
@@ -3,9 +3,9 @@ const { ERROR_INTERNAL_SERVER_ERROR } = require('./errors');
 module.exports.handleOtherErrors = (err, req, res, next) => {
   const { statusCode = ERROR_INTERNAL_SERVER_ERROR, message } = err;
   res
-    .status(ERROR_INTERNAL_SERVER_ERROR)
+    .status(statusCode)
     .send({
-      message: statusCode === 500
+      message: statusCode === ERROR_INTERNAL_SERVER_ERROR
         ? 'На сервере произошла ошибка'
         : message,
     });
